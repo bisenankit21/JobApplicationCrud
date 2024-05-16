444fc025129a8cdd9c371888eed2db1bf93cda9e
AnkitBisen <bisenankit21@gmail.com>
2024-05-16 10:57:13 +0530

    Initial commit

---
+++ src/main/java/com/example/springboot/JobApplicationCrud/JobApplicationCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.JobApplicationCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobApplicationCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobApplicationCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot/JobApplicationCrud/JobApplicationCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.JobApplicationCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobApplicationCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
